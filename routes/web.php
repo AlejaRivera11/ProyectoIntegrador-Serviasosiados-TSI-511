25874d79f2ae7ee5a5c8b4d968d79d4538517dc4
AlejaRivera11 <ariveram@estudiante.uniajc.edu.co>
2026-04-11 15:28:25 -0500

    Creacion de tabla BDD  y Modelo Cliente

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\VehiculoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/cliente',ClienteController::class);
+
+Route::resource('/vehiculo',VehiculoController::class);
+
